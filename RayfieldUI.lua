7c2a64b21ed9235ff98e4e08d5865c710c6df29c
Usung <110562178+chwhil1134@users.noreply.github.com>
2026-03-18 19:10:16 +0900

    레이필드 UI 숨기기 수정
    UI 숨기기 후 다시 띄울 때 마지막으로 숨기기한 곳에 UI를 띄울 수 있도록 수정.

--- RayfieldUI.lua
+++ RayfieldUI.lua
@@ -1354,7 +1354,7 @@ end
 
 local function Unhide()
 	Debounce = true
-	Main.Position = UDim2.new(0.5, 0, 0.5, 0)
+	-- Main.Position = UDim2.new(0.5, 0, 0.5, 0)
 	Main.Visible = true
 	TweenService:Create(Main, TweenInfo.new(0.5, Enum.EasingStyle.Exponential), {Size = useMobileSizing and UDim2.new(0, 500, 0, 275) or UDim2.new(0, 500, 0, 475)}):Play()
 	TweenService:Create(Main.Topbar, TweenInfo.new(0.5, Enum.EasingStyle.Exponential), {Size = UDim2.new(0, 500, 0, 45)}):Play()
